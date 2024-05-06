34d2af213885318683686e5df3909ee50aeb9095
unknown <lakku-lakshmi>
2024-05-06 22:14:57 +0530

    four

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,9 @@
-// App.test.js
-
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from './App'; // Import the App component
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-test('renders logo', () => {
-  const { getByAltText } = render(<App />);
-  const logoElement = getByAltText('logo');
-  expect(logoElement).toBeInTheDocument();
 });
