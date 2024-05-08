91e15cfa48d1bca19d33e2db262167d96d820c7f
unknown <lakku-lakshmi>
2024-05-08 14:44:08 +0530

    only Learn

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learnnnnn reacttt ssss
+          Learn
         </a>
       </header>
     </div>
